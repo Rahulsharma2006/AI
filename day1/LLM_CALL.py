cc8e1cf5146b3b1c5843defe07529a50f34c3f22
Rahul Sharma <rs0987jrkg@gmail.com>
2026-07-15 04:48:13 +0530

    First LLM

---
+++ day1/LLM_CALL.py
@@ -0,0 +1,24 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from groq import Groq
+
+#   Config DOTEnV File
+load_dotenv()
+my_api_key = os.getenv("GROQ_API_KEY")
+if not my_api_key:
+    raise ValueError("API Error")
+client = Groq(api_key=my_api_key)
+model="llama-3.3-70b-versatile"
+role="user"
+prompt=input("Type To Search : ")
+message = {
+    "role":role,
+    "content":prompt
+}
+messages = [message]
+response = client.chat.completions.create(
+    model=model,
+    messages=messages
+)
+print(response.choices[0].message.content)
